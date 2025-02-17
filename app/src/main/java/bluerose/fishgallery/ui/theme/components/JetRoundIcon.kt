9da8bfddc26f08e9ca24b5a9800d9ab20f117a97
Shevikina <Shevik.vlog@gmail.com>
2025-02-17 17:42:31 +0300

    Update JetRoundIcon

--- app/src/main/java/bluerose/fishgallery/ui/theme/components/JetRoundIcon.kt
+++ app/src/main/java/bluerose/fishgallery/ui/theme/components/JetRoundIcon.kt
@@ -1,13 +1,11 @@
 package bluerose.fishgallery.ui.theme.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import bluerose.fishgallery.R
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,11 +15,17 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import bluerose.fishgallery.R
 import bluerose.fishgallery.ui.theme.FishGalleryTheme
 
 @Composable
-fun JetRoundIcon(vectorDrawableId: Int, color: Color, modifier: Modifier = Modifier) {
+fun JetRoundIcon(
+    vectorDrawableId: Int,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
     Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
             .clip(shape = CircleShape)
             .border(
@@ -29,13 +33,11 @@ fun JetRoundIcon(vectorDrawableId: Int, color: Color, modifier: Modifier = Modif
                 color = color,
                 shape = CircleShape
             )
-            .size(64.dp),
-        contentAlignment = Alignment.Center
     ) {
         Icon(
             imageVector = ImageVector.vectorResource(id = vectorDrawableId),
-            contentDescription = "fish icon",
-            tint = color
+            tint = color,
+            contentDescription = null
         )
     }
 }
@@ -47,7 +49,7 @@ fun JetRoundIconPreview() {
         JetRoundIcon(
             vectorDrawableId = R.drawable.ic_icon,
             color = MaterialTheme.colorScheme.onPrimary,
-            modifier = Modifier
+            modifier = Modifier.size(64.dp)
         )
     }
 }
