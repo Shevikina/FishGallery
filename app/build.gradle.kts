f1705c14c866e51648bf8371759695568b9fcf06
Shevikina <Shevik.vlog@gmail.com>
2025-04-21 23:17:12 +0300

    Upd navigation version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation("io.coil-kt:coil:2.5.0")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.9")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("com.microsoft.design:fluent-system-icons:1.1.245@aar")
 
