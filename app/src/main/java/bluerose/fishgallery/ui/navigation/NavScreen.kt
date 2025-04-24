1528c256dffd82f2d84170ad986b6b16993fa952
Shevikina <Shevik.vlog@gmail.com>
2025-04-24 18:59:51 +0300

    Fix NavScreen routes

--- app/src/main/java/bluerose/fishgallery/ui/navigation/NavScreen.kt
+++ app/src/main/java/bluerose/fishgallery/ui/navigation/NavScreen.kt
@@ -1,6 +1,12 @@
 package bluerose.fishgallery.ui.navigation
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed class NavScreen {
+    @Serializable
     data object Statistics : NavScreen()
-    data object Catch: NavScreen()
+
+    @Serializable
+    data object Catch : NavScreen()
 }
